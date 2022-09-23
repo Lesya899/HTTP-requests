610c9ee973743400c7f14bbaa12befbe40a13a40
Олеся Николаева <Niki9180@mail.ru>
2022-09-23 21:10:40 +0300

    comment added

--- src/YandexMaps/FortressWall.java
+++ src/YandexMaps/FortressWall.java
@@ -1,4 +1,8 @@
+/*Напишите программу с использованием графического интерфейса, которая отобразит на карте Крепостную смоленскую стену.
+ Добавьте метку на любые 5 башен на ваш вкус*/
+
 package YandexMaps;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -20,8 +24,8 @@ public class FortressWall extends JFrame{
                 Image img = ImageIO.read(new URL("https://static-maps.yandex.ru/1.x/?ll=32.041296,54.781810&&spn=0.003,0.003&l=map&" +
                         "pt=32.041901,54.780564,pmvvm1~32.040087,54.781271,pmb~32.043756,54.780070,pm2dgm3~32.045687,54.779619,flag~32.041665,54.780866,pm2blywm5"));
                 this.label.setIcon(new ImageIcon(img));
-            } catch (IOException var3) {
-                throw new RuntimeException(var3);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
         setPreferredSize(new Dimension(900, 700));
