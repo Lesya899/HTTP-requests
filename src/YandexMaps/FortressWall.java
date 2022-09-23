6939a4438ef1f578315b06798d8cbfee93da5e27
Олеся Николаева <Niki9180@mail.ru>
2022-09-23 18:40:33 +0300

    using labels in the Static API

---
+++ src/YandexMaps/FortressWall.java
@@ -0,0 +1,40 @@
+package YandexMaps;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+
+public class FortressWall extends JFrame{
+    private JPanel panel;
+    private JLabel label;
+    private JButton button;
+
+
+    public FortressWall()  {
+        super("Смоленская крепостная стена");
+        button.addActionListener((e) -> {
+            try {
+                Image img = ImageIO.read(new URL("https://static-maps.yandex.ru/1.x/?ll=32.041296,54.781810&&spn=0.003,0.003&l=map&" +
+                        "pt=32.041901,54.780564,pmvvm1~32.040087,54.781271,pmb~32.043756,54.780070,pm2dgm3~32.045687,54.779619,flag~32.041665,54.780866,pm2blywm5"));
+                this.label.setIcon(new ImageIcon(img));
+            } catch (IOException var3) {
+                throw new RuntimeException(var3);
+            }
+        });
+        setPreferredSize(new Dimension(900, 700));
+        getContentPane().add(panel);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FortressWall();
+    }
+}
+
+
